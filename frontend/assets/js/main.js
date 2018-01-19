1bcc14e1d9f28cef720c27e6cb4cffcdaf7dfa92
mKatrichenko <m_katrihenko@ukr.net>
2018-01-19 21:29:01 +0200

    my first commit

---
+++ frontend/assets/js/main.js
@@ -0,0 +1,12 @@
+/*$('.slide-wrapper').slick({
+    dots: true,
+    arrows: false
+});*/
+
+/*$('box').on('mouseover', function(){
+  $(this).parents('.pricing').css({'padding-bottom': '144px', 'padding-top': '87px'});
+});*/
+
+/*$('.pricing .wrapper .box-1').hover(function(){
+  $(this).parents('.pricing').css({'padding-bottom': '144px', 'padding-top': '87px'});
+});*/
